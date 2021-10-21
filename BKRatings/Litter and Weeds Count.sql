efb2f64d43cb8743f55217708411b65c9fa64b61
Jasmine Soltani <jasmine.soltani@parks.nyc.gov>
2021-10-21 15:28:49 -0400

    document query changes based on underlying pip table changes

--- BKRatings/Litter and Weeds Count.sql
+++ BKRatings/Litter and Weeds Count.sql
@@ -1,38 +1,48 @@
---Acceptable Litter & Weeds Season to Date for BK Ratings Report published each round
---Last modified: 3/17/2021
+-- Acceptable Litter & Weeds Season to Date for BK Ratings Report published each round
+-- Modified 6/22/2021 to compare with 2019 summer season rather than 2020
+-- Modified 7/20/2020 AMPSDistrict should always be used to avoid legacy errors with DISTRICT
+-- Modified 10/21/2021 b/c underlying tables changed - joined on Inspection Main & All Sites to filter to Borough and group by district
 
 USE [DWH]
 
 --LITTER
-SELECT [Inspection Year]
-,[District]
-,[Rating]
-,count(case when [Inspection Year]=2020 and (Round in ('1')) then Feature									 --previous year to date 
-	        when [Inspection Year]=2021 and (Round in ('1')) then Feature else null end) AS 'Litter Count'   --current year season to date
-  FROM [dbo].[tbl_PIP_FeatureRatings]
-  where Borough = 'Brooklyn'
-  and [Inspection Year] in (2020, 2021)						--update years
-  and Season ='Spring'					                    --update season
+SELECT Main.[Inspection Year]
+,Main.[AMPSDistrict]
+,Feature.[Rating]
+,count(case when Main.[Inspection Year]=2019 and (Round in ('1', '2', '3')) then Feature.Feature									 --previous year to date 
+	        when Main.[Inspection Year]=2021 and (Round in ('1', '2', '3')) then Feature.Feature else null end) AS 'Litter Count'   --current year season to date
+  FROM [dbo].[tbl_PIP_InspectionMain] Main
+  FULL OUTER JOIN [dbo].[tbl_PIP_FeatureRatings] Feature
+  ON (Main.[Inspection ID] = Feature.[Inspection ID])
+  FULL OUTER JOIN [dbo].[TBL_PIP_ALLSITES] AllSites
+  ON (Main.[Prop ID] = AllSites.[PropID])
+  where AllSites.Boro = 'B'
+  and [Inspection Year] in (2019, 2021)						--update years
+  and Season ='Fall'					                    --update season
   and Feature = 'Litter'
   and Rating <> 'N'
-  and InspectionType = 'PIP'
-group by Rating, District, [Inspection Year]
-order by District, [Inspection Year], Rating
+  --and InspectionType = 'PIP'
+group by Feature.Rating, Main.AMPSDistrict, Main.[Inspection Year]
+order by Main.AMPSDistrict, Main.[Inspection Year], Feature.Rating
 ;
 
 --WEEDS (or other feature of interest)
-SELECT [Inspection Year]
-,[District]
-,[Rating]
-,count(case when [Inspection Year]=2020 and (Round in ('1')) then Feature										--previous year to date
-	        when [Inspection Year]=2021 and (Round in ('1')) then Feature else null end) AS 'Paved Surfaces'	--current year season to date
-  FROM [dbo].[tbl_PIP_FeatureRatings]
-  where Borough = 'Brooklyn'
-  and [Inspection Year] in (2020, 2021)						--update years
-  and Season ='Spring'					                    --update season
-  and Feature = 'Paved Surfaces'							--feature to query
+SELECT Main.[Inspection Year]
+,Main.[AMPSDistrict]
+,Feature.[Rating]
+,count(case when Main.[Inspection Year]=2019 and (Round in ('1', '2', '3')) then Feature.Feature									 --previous year to date 
+	        when Main.[Inspection Year]=2021 and (Round in ('1', '2', '3')) then Feature.Feature else null end) AS 'Weeds Count'   --current year season to date
+  FROM [dbo].[tbl_PIP_InspectionMain] Main
+  FULL OUTER JOIN [dbo].[tbl_PIP_FeatureRatings] Feature
+  ON (Main.[Inspection ID] = Feature.[Inspection ID])
+  FULL OUTER JOIN [dbo].[TBL_PIP_ALLSITES] AllSites
+  ON (Main.[Prop ID] = AllSites.[PropID])
+  where AllSites.Boro = 'B'
+  and [Inspection Year] in (2019, 2021)						--update years
+  and Season ='Fall'					                    --update season
+  and Feature = 'Weeds'
   and Rating <> 'N'
-  and InspectionType = 'PIP'
-group by Rating, District, [Inspection Year]
-order by District, [Inspection Year], Rating
+  --and InspectionType = 'PIP'
+group by Feature.Rating, Main.AMPSDistrict, Main.[Inspection Year]
+order by Main.AMPSDistrict, Main.[Inspection Year], Feature.Rating
 ;
--- BKRatings/Ratings Percentage by Districts.sql
+++ BKRatings/Ratings Percentage by Districts.sql
@@ -1,43 +1,45 @@
 --Overall Condition & Cleanliness percentages Season to Date for BK Ratings Report published each round
---Last modified: 3/17/2020
+-- Modified: 6/22/2021 to compare with 2019 summer season rather than 2020
+-- Modified 7/20/2020 AMPSDistrict should always be used to avoid legacy errors with DISTRICT
+-- Modified 10/21/2021 b/c underlying tables changed - variable names changed slightly and InspectionType variable no longer exists
 
 USE [DWH]
 GO
 
 SELECT	SecondSet.District_2021, --update year names each new inspection year
-	  FirstSet.District_2020,
+	  FirstSet.District_2019,
 	  SecondSet.[2021 OC],
-	  FirstSet.[2020 OC],
+	  FirstSet.[2019 OC],
 	  SecondSet.[2021 C],
-	  FirstSet.[2020 C]
+	  FirstSet.[2019 C]
 FROM (SELECT
-		District as District_2020, 
-		CASE -- 2020 OC:
+		Main.[AMPSDistrict] as District_2019, 
+		CASE -- 2019 OC:
 			WHEN COUNT(CASE WHEN [Overall Condition] IN ('A','U','U/S') THEN [Overall Condition] ELSE NULL END) = 0 --check if denominator is ZERO 
 			THEN NULL -- return null otherwise divide for a percentage:
 			ELSE CAST(ROUND(COUNT(CASE WHEN [Overall Condition] = 'A' THEN [Overall Condition] ELSE NULL END) *100.0 /
 						   (COUNT(CASE WHEN [Overall Condition] IN ('A','U','U/S') THEN [Overall Condition] ELSE NULL END)),1)as decimal (5,1)) 
-			END AS [2020 OC],
-		CASE -- 2020 C
+			END AS [2019 OC],
+		CASE -- 2019 C
 			WHEN COUNT(CASE WHEN [Cleanliness] IN ('A','U','U/S') THEN [Cleanliness] ELSE NULL END) = 0 --check if denominator is ZERO 
 			THEN NULL -- return null otherwise divide for a percentage:
 			ELSE CAST(ROUND((COUNT(CASE WHEN [Cleanliness] = 'A' THEN [Cleanliness] ELSE NULL END)) *100.0/
 				  (COUNT(CASE WHEN [Cleanliness] IN ('A','U','U/S') THEN [Cleanliness] ELSE NULL END)),1)as decimal (5,1)) 
-			END AS [2020 C]
-	  FROM [dbo].[tbl_PIP_InspectionMain]
-	  FULL OUTER JOIN tbl_PIP_AllSites
-	  ON (tbl_PIP_AllSites.[Prop ID] = tbl_PIP_InspectionMain.[Prop ID] )
+			END AS [2019 C]
+	  FROM [dbo].[tbl_PIP_InspectionMain] Main
+	  FULL OUTER JOIN tbl_PIP_AllSites AllSites
+	  ON (AllSites.[PropID] = Main.[Prop ID] )
 	  WHERE Boro = 'B'
-	  and InspectionType ='PIP'
-	  and [Inspection Year]= '2020'			--previous year
-	  and Season ='Spring'					--update season
-	  and Round IN ('1')					--update ROUND TO DATE
-	  GROUP BY District
+	  --and InspectionType ='PIP'			--variable no longer available, all PIP?
+	  and [Inspection Year]= '2019'			--previous year
+	  and Season ='Fall'					--update season
+	  and Round IN ('1', '2', '3')					--update ROUND TO DATE
+	  GROUP BY Main.AMPSDistrict
 	  )	
   AS FirstSet
 FULL OUTER JOIN 
 	(SELECT
-	   District as District_2021,
+	   Main.AMPSDistrict as District_2021,
 		CASE -- 2021 OC:
 			WHEN COUNT(CASE WHEN [Overall Condition] IN ('A','U','U/S') THEN [Overall Condition] ELSE NULL END) = 0 --check if denominator is ZERO 
 			THEN NULL -- return null otherwise divide for a percentage:
@@ -50,19 +52,19 @@ FULL OUTER JOIN
 			ELSE CAST(ROUND((COUNT(CASE WHEN [Cleanliness] = 'A' THEN [Cleanliness] ELSE NULL END)) *100.0/
 				  (COUNT(CASE WHEN [Cleanliness] IN ('A','U','U/S') THEN [Cleanliness] ELSE NULL END)),1)as decimal (5,1)) 
 			END AS [2021 C]
-	  FROM [dbo].[tbl_PIP_InspectionMain]
-	  FULL OUTER JOIN tbl_PIP_AllSites
-	  ON (tbl_PIP_AllSites.[Prop ID] = tbl_PIP_InspectionMain.[Prop ID] )
+	  FROM [dbo].[tbl_PIP_InspectionMain] Main
+	  FULL OUTER JOIN tbl_PIP_AllSites AllSites
+	  ON (AllSites.[PropID] = Main.[Prop ID] )
 	  WHERE Boro = 'B'
-	  and InspectionType ='PIP'
+	  --and InspectionType ='PIP'				--variable no longer available, all PIP?
 	  and [Inspection Year]= '2021'				--current year
-	  and Season ='Spring'						--update season
-	  and Round IN ('1')						--update ROUND TO DATE
-	  GROUP BY District
+	  and Season ='Fall'						--update season
+	  and Round IN ('1', '2', '3')						--update ROUND TO DATE
+	  GROUP BY Main.AMPSDistrict
 	  ) 
  AS SecondSet
- ON FirstSet.District_2020 = SecondSet.District_2021
- ORDER BY Firstset.District_2020
+ ON FirstSet.District_2019 = SecondSet.District_2021
+ ORDER BY Firstset.District_2019
 GO
 
 
